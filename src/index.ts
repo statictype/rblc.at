5a40a1679663938e658b875a9d480ca796eef47c
Andreea Eftene <hi@pixelle.ro>
2025-05-18 00:17:27 +0300

    avoid fouc and simplfy animation

--- src/index.ts
+++ src/index.ts
@@ -3,88 +3,110 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin);
 
-let tl = gsap.timeline({});
+let tl = gsap.timeline({ defaults: { opacity: 0 } });
 
-tl.fromTo(
-  "#deco_triangle",
-  { scaleY: 0, transformOrigin: "left bottom" },
-  { scaleY: 1, duration: 1, ease: "power4.in" }
-)
-  .fromTo(
-    "#deco_ellipse_small",
-    { scale: 0, transformOrigin: "center" },
-    { scale: 1, duration: 0.5 }
-  )
-  .fromTo(
-    "[id^='deco_line']",
-    { scaleY: 0, transformOrigin: "top center" },
-    { scaleY: 1, duration: 0.5, stagger: 0.2, ease: "power4.in" },
-    "-=1.5"
-  )
-  .fromTo(
-    "#deco_ellipse_big",
-    { y: -400 },
-    { y: 1, duration: 1, ease: "elastic.out(1,0.3)" },
-    "-=0.5"
-  )
-  .fromTo(
-    "#ear_left",
-    { scaleY: 0, transformOrigin: "left bottom" },
-    { scaleY: 1, duration: 1, ease: "expo.out" },
-    "-=0.8"
-  )
-  .fromTo(
-    "#ear_right",
-    { scaleY: 0, transformOrigin: "right bottom" },
-    { scaleY: 1, duration: 1, ease: "expo.out" },
-    "-=1"
-  )
-  .fromTo("#neck", { y: 200 }, { y: 1, duration: 1, ease: "expo.out" }, "-=1")
-  .fromTo(
-    "#vesica",
-    { scaleY: 0, transformOrigin: "center" },
-    { scaleY: 1, duration: 0.5, ease: "expo.out" },
-    "-=1"
-  )
-  .fromTo(
-    "#nose",
-    { scale: 0, transformOrigin: "bottom center" },
-    { scale: 1, duration: 0.3, ease: "power4.out" },
-    "-=0.8"
-  )
-  .fromTo(
-    "#eye_left, #eye_right",
-    { scaleY: 0, transformOrigin: "center" },
-    { scaleY: 1, duration: 1, ease: "expo.out" },
-    "-=0.8"
-  )
-  .fromTo(
-    "#mouth_left, #mouth_right",
-    { drawSVG: 0 },
-    { drawSVG: "100%", duration: 0.5 },
-    "-=1"
-  )
-  .fromTo(
-    "#whiskers_left path",
-    { scaleX: 0, transformOrigin: "right bottom" },
-    { scaleX: 1, duration: 1, stagger: 0.2, ease: "elastic.out(1,0.3)" },
-    "-=0.8"
-  )
-  .fromTo(
-    "#whiskers_right path",
-    { scaleX: 0, transformOrigin: "left bottom" },
-    { scaleX: 1, duration: 1, stagger: 0.2, ease: "elastic.out(1,0.3)" },
-    "-=1.6"
-  )
-  .fromTo(
-    "#eye_left, #eye_right",
-    { scaleY: 0, transformOrigin: "center" },
-    {
-      scaleY: 1,
+function init() {
+  tl.from(".cat", { autoAlpha: 0 })
+    .from("#deco_triangle", {
+      scaleY: 0,
+      transformOrigin: "left bottom",
       duration: 1,
-      ease: "expo.out",
-      repeat: -1,
-      repeatDelay: 10,
-    },
-    "-=0.4"
-  );
+      ease: "power4.in",
+    })
+    .from("#deco_ellipse_small", {
+      scale: 0,
+      transformOrigin: "center",
+      duration: 0.5,
+    })
+    .from(
+      "[id^='deco_line']",
+      {
+        scaleY: 0,
+        transformOrigin: "top center",
+        duration: 0.5,
+        stagger: 0.2,
+        ease: "power4.in",
+      },
+      "-=1.5"
+    )
+    .from(
+      "#deco_ellipse_big",
+      { y: -400, duration: 1, ease: "elastic.out(1,0.3)" },
+      "-=0.5"
+    )
+    .from(
+      "#ear_left",
+      {
+        scaleY: 0,
+        transformOrigin: "left bottom",
+        duration: 1,
+        ease: "expo.out",
+      },
+      "-=0.8"
+    )
+    .from(
+      "#ear_right",
+      {
+        scaleY: 0,
+        transformOrigin: "right bottom",
+        duration: 1,
+        ease: "expo.out",
+      },
+      "-=1"
+    )
+    .from("#neck", { y: 200, duration: 1, ease: "expo.out" }, "-=1")
+    .from(
+      "#vesica",
+      { scaleY: 0, transformOrigin: "center", duration: 0.5, ease: "expo.out" },
+      "-=1"
+    )
+    .from(
+      "#nose",
+      {
+        scale: 0,
+        transformOrigin: "bottom center",
+        duration: 0.3,
+        ease: "power4.out",
+      },
+      "-=0.8"
+    )
+    .from(
+      "#eye_left, #eye_right",
+      {
+        scaleY: 0,
+        transformOrigin: "center",
+        duration: 1,
+        ease: "expo.out",
+        repeat: -1,
+        repeatDelay: 5,
+      },
+      "-=0.8"
+    )
+    .from("#mouth_left, #mouth_right", { drawSVG: 0, duration: 0.5 }, "-=1")
+    .from(
+      "#whiskers_left path",
+      {
+        scaleX: 0,
+        transformOrigin: "right bottom",
+        duration: 1,
+        stagger: 0.2,
+        ease: "elastic.out(1,0.3)",
+      },
+      "-=0.8"
+    )
+    .from(
+      "#whiskers_right path",
+      {
+        scaleX: 0,
+        transformOrigin: "left bottom",
+        duration: 1,
+        stagger: 0.2,
+        ease: "elastic.out(1,0.3)",
+      },
+      "-=1.6"
+    );
+}
+
+window.addEventListener("load", function (event) {
+  init();
+});
